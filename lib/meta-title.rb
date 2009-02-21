f5de7f00cda6afb3170bdb88ecb813ae4470bf51
U-FABIONUDIBRANCH\rjp <zimpenfish@gmail.com>
2009-02-21 09:39:29 +0000

    - move to using libextractor (via the extractor ruby shim) for metadata - get the first 16k, write to a tempfile, use extractor - add some more test URLs for testing the tests

--- lib/meta-title.rb
+++ lib/meta-title.rb
@@ -1,7 +1,8 @@
 require 'net/http'
 require 'open-uri'
 require 'rubygems'
-require 'hpricot'
+require 'extractor'
+require 'tempfile'
 
 def title(uri)
     x=''
@@ -27,19 +28,31 @@ def title_x(uri)
 
     puts "GET"
     req = Net::HTTP::Get.new(url.path)
-    req.set_range(0, 1023)
+    req.set_range(0, 16383)
     res = Net::HTTP.start(url.host, url.port) {|x|
         x.request(req)
     }
     puts res.code
-    puts res.content_length
-    puts res.body.length
-    if res.content_type == 'text/html' then
-        a = Hpricot(res.body)
-        p a.search('title').inner_text.strip
-        p a.inner_text.length
+    puts res.content_type
+    body = res.body
     end
+
+    # some formats need the end of the file as well
+    # how should we decide which those are?
+
+    # write the body to a tempfile
+    Tempfile.open('lnkylnky', '/tmp') { |t|
+        t.print body
+        t.flush
+        metadata = Extractor.extract(t.path)
+        p metadata
+        t.close!
+    }
 end
 
+title_x('http://rjp.frottage.org/tmp/sorry.mp3')
+title_x('http://rjp.frottage.org/tmp/theme2.mp3')
+title_x('http://rjp.frottage.org/catsink.jpg')
+title_x('http://rjp.frottage.org/uaposts.png')
 title_x('http://frottage.org/mysql/manual.html')
 title_x('http://backup.frottage.org/rjp/env.cgi')
