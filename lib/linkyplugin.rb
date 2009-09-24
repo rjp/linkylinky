6d5ce93ff9ac274812f884ba0d767f2bcdef7b99
rjp <zimpenfish@gmail.com>
2009-09-24 20:29:25 +0100

    - move to alternate way of handling plugins - references to the objects are stored in a hash by plugin name - accept() checks a uri against match_uri and negative_match_uri - TODO handle match_uri & negative_match_uri being arrays - move LinkyLinky.init_plugins to linkyplugin rather than meta-title

--- lib/linkyplugin.rb
+++ lib/linkyplugin.rb
@@ -13,12 +13,45 @@ module PluginSugar
   end
 end
 
-class PluginBase
-  class << self
+class Plugin
+    @registered = {}
+    class << self
+        attr_reader :registered
+        private :new
+    end
+
+    def self.define(name, &block)
+        p = new
+        p.instance_eval(&block)
+        Plugin.registered[name] = p
+    end
+
+    def accept(uri)
+        allowed = 0
+
+        unless @match_uri.nil? then
+            if uri.match(Regexp.new(@match_uri)) then
+                allowed = 1
+            end
+        end
+        unless @negative_match_uri.nil? then
+            if uri.match(Regexp.new(@negative_match_uri)) then
+                allowed = 0
+            end
+        end
+
+        return allowed
+    end
+
     extend PluginSugar
     def_field :author, :version
     def_field :match_host, :match_uri
     def_field :negative_match_uri
-  end
+end
+
+module LinkyLinky
+	def init_plugins(dir)
+	    Dir.glob(dir + '/*.rb').each { |rb| load rb }
+	end
 end
 
