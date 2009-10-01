7c1700556c076e6bfcb12e369502f6384f444160
rjp <zimpenfish@gmail.com>
2009-10-01 08:29:37 +0100

    - flickr: rationalise the unit tests

--- lib/plugins/flickr.rb
+++ lib/plugins/flickr.rb
@@ -66,15 +66,12 @@ class TC_plugin_flickr < Test::Unit::TestCase
     def test_flickr_online
         return unless ENV['LL_ONLINE']
 
-        uri = 'http://www.flickr.com/photos/8279638@N05/3877512686/'
-        a = @flickr.fetch(uri, 'text/html', 1234, '')
+        a = @flickr.fetch('http://www.flickr.com/photos/8279638@N05/3877512686/', 'text/html', 1234, '') 
         assert_not_nil(a)
-# should the fetch return the uri as well?  would make this a bit less messy
-        assert_not_nil(@flickr.title(uri, *a))
+        assert_not_nil(@flickr.title(*a))
 
-        uri = 'http://www.flickr.com/photos/eric_parey/3877173831/'
-        a = @flickr.fetch(uri, 'text/html', '1234', '')
+        a = @flickr.fetch('http://www.flickr.com/photos/eric_parey/3877173831/', 'text/html', 1234, '')
         assert_not_nil(a)
-        assert_not_nil(@flickr.title(uri, *a))
+        assert_not_nil(@flickr.title(*a))
     end
 end
