5e437c6382eac17f0d4f1bc80811ea46860b2c33
rjp <zimpenfish@gmail.com>
2009-10-01 07:13:24 +0100

    - flickr: update unit tests - TODO consider returning the uri from the .fetch method so you can pass that straight to the .title method - TODO also allows internal uri rewriting (static flickr image => flickr photo page for title?)

--- lib/plugins/flickr.rb
+++ lib/plugins/flickr.rb
@@ -59,14 +59,22 @@ class TC_plugin_flickr < Test::Unit::TestCase
     end
 
     def test_flickr_accept
-        assert_equal(@flickr.accept('flickr.com/photos/cock/weasel'), true)
-        assert_equal(@flickr.accept('flickr.com/cock/weasel'), false)
+        assert_equal(true, @flickr.accept('flickr.com/photos/monkey/weasel', 'text/html'))
+        assert_equal(false, @flickr.accept('flickr.com/monkey/weasel', 'text/html'))
     end
 
     def test_flickr_online
         return unless ENV['LL_ONLINE']
 
-        assert_not_nil(@flickr.title('http://www.flickr.com/photos/8279638@N05/3877512686/'))
-        assert_not_nil(@flickr.title('http://www.flickr.com/photos/eric_parey/3877173831/'))
+        uri = 'http://www.flickr.com/photos/8279638@N05/3877512686/'
+        a = @flickr.fetch(uri, 'text/html', 1234, '')
+        assert_not_nil(a)
+# should the fetch return the uri as well?  would make this a bit less messy
+        assert_not_nil(@flickr.title(uri, *a))
+
+        uri = 'http://www.flickr.com/photos/eric_parey/3877173831/'
+        a = @flickr.fetch(uri, 'text/html', '1234', '')
+        assert_not_nil(a)
+        assert_not_nil(@flickr.title(uri, *a))
     end
 end
